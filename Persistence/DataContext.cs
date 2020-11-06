450694408bc04923908714031f11bb0745f10e7c
Freixes Sanjuan <marc.freixes@sage.com>
2020-11-06 17:11:57 +0100

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "Valor 101" },
+                    new Value { Id = 2, Name = "Valor 102" },
+                    new Value { Id = 3, Name = "Valor 103" }
+                );
+        }
+    }
+}
---
+++ Persistence/Migrations/20200925141410_SeedValues.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class SeedValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Valor 101" }
+                );
+
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Valor 102" }
+                );
+
+            migrationBuilder.InsertData(
+                table: "Values",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Valor 103" }
+                );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
